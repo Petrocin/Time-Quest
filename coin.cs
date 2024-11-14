404773905fe6f65ff3767b86b94962af9ef2f15a
Alexander Gordienko <distowin2288@gmail.com>
2024-11-14 20:36:01 +0300

    Update coin.cs
    поправил логику алгоритма

--- coin.cs
+++ coin.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class coin : MonoBehaviour
+public class Coin : MonoBehaviour 
 {
     public AudioClip collectSound;
     private AudioSource audioSource;
-  
+    public AudioClip coinSound;
+
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
-        audioSource.Play();
         audioSource.clip = collectSound;
-      
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
             PlayCollectSound();
-            audioSource.Play();
             CoinCounter.Instance.CollectCoin();
             Destroy(gameObject);
         }
@@ -27,14 +27,9 @@ public class coin : MonoBehaviour
     void PlayCollectSound()
     {
         Debug.Log("Playing collect sound.");
-        audioSource.Play();
-        audioSource.PlayOneShot(collectSound);
-        audioSource.volume = 1.0f;
+        AudioSource.PlayClipAtPoint(coinSound, transform.position);
         Debug.Log("Played sound: " + collectSound.name);
     }
 }
 
-
-
-
-
+// поправил логику алгоритма
