e9b81f49e1a4a35ef9bdae26ec8e5f0f88a290fa
Alexander Gordienko <distowin2288@gmail.com>
2024-11-17 00:01:20 +0300

    Add files via upload

---
+++ QuestUIManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class QuestUIManager : MonoBehaviour
+{
+    public QuestManager questManager; 
+    public GameObject questItemPrefab; 
+    public Transform content; 
+
+    void Start()
+    {
+        if (questManager == null)
+        {
+            questManager = FindObjectOfType<QuestManager>();
+        }
+
+        UpdateQuestList();
+    }
+
+    public void UpdateQuestList()
+    {
+
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+
+ 
+        foreach (var quest in questManager.quests)
+        {
+            GameObject questItem = Instantiate(questItemPrefab, content);
+            Text questText = questItem.GetComponent<Text>();
+            questText.text = $"{quest.questName} - {quest.currentAmount}/{quest.targetAmount} {(quest.isCompleted ? "(Завершен)" : "")}";
+        }
+    }
+}
